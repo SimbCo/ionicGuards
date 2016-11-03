9b6c4f4b1891831b9174de35ecef036d51557416
Olivia Hinton <hinton.olivia@gmail.com>
2016-11-03 10:30:36 -0700

    add functionality for nav push guards on leaving and not leaving a page

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ListPage } from '../list/list';
 
 
 @Component({
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  canLeave: boolean;
 
+  constructor(public navCtrl: NavController) {
   }
+
+  doCanLeave(){
+    this.canLeave = true;
+    this.navCtrl.push(ListPage)
+     .catch(()=> console.log('I can leave?'))
+  }
+
+  canNotLeave(){
+    this.canLeave = false;
+    this.navCtrl.push(ListPage)
+     .catch(()=> console.log('I can not leave?'))
+  }
+
+  ionViewCanLeave(): boolean{
+    return this.canLeave;
+   }
+
 }
